2af869a085931a977d09f258f3df4ec54d775b22
Anastasiia <anastasiia0matsiuk@gmail.com>
2026-06-19 23:03:05 +0300

    fix name file noteStore and update NoteForm

--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -5,7 +5,7 @@
 import { useRouter } from "next/navigation";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createNote } from "@/lib/api";
-import { useNoteStore } from "@/lib/store/NoteStore";
+import { useNoteStore } from "@/lib/store/noteStore";
 import type { NoteTag } from "@/types/note";
 
 import css from "./NoteForm.module.css";
